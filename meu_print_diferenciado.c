cbc11f170fefaf47298ec9b5c9c439165d0a50a7
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-07-28 22:11:59 -0300

    Umas doideras com macros pra fazer um print diferenciado

--- meu_print_diferenciado.c
+++ meu_print_diferenciado.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 
 /*
-	Umas doideras com macros o.O
+  Umas doideras com macros o.O
 */
 
 #define formato(x)   \
-	_Generic(x,        \
-		char*:  "%s\n",  \
-		char:   "%c\n",  \
-		double: "%f\n",  \
-		int:    "%d\n"   \
-)  
+  _Generic(x,        \
+    char*:  "%s\n",  \
+    char:   "%c\n",  \
+    double: "%f\n",  \
+    int:    "%d\n"   \
+)
 
 #define tamanho_array(x)\
-	sizeof(x) / sizeof(x[0]) 
+  sizeof(x) / sizeof(x[0]) 
 
 #define meu_print_diferenciado(T, ...) {         \
-	T array[] = {__VA_ARGS__};                     \
-	for(int x = 0; x < tamanho_array(array); x++)  \
-		printf(formato(array[x]), array[x]);         \
-	putchar('\n');                                 \
+  T array[] = {__VA_ARGS__};                     \
+  for(int x = 0; x < tamanho_array(array); x++)  \
+    printf(formato(array[x]), array[x]);         \
+  putchar('\n');                                 \
 }
 
 int main() {
 
-	meu_print_diferenciado(char, 65, 66, 67, 78);
-	meu_print_diferenciado(int, 65, 'a', 67, 78, 'B');
-	meu_print_diferenciado(char*, "Hello", "World!", ":)");
-	meu_print_diferenciado(double, 80.1);
+  meu_print_diferenciado(char, 65, 66, 67, 78);
+  meu_print_diferenciado(int, 65, 'a', 67, 78, 'B');
+  meu_print_diferenciado(char*, "Hello", "World!", ":)");
+  meu_print_diferenciado(double, 80.1);
 
 }
