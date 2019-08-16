a0cc803782ce6efe031f656210db00bba91d1515
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-08-15 23:02:45 -0300

    Usando _Person.new para criar instancia da classe e _Person.delete para destruir a instancia da classe. Metodo info agora passa a ser exclusivo da classe

--- poo-c/testes/main.c
+++ poo-c/testes/main.c
@@ -2,26 +2,24 @@
 #include <stdio.h>
 #include "interface.h"
 
-// dps vou por isso em _Person, por enquanto vai ficar como extern :]
-extern void* new(char* const this);
-extern void _info(const struct person* const this);
-
 int main() {
 
-  Person* p = (Person*) new("Gabriel");
-  _info(p);
+  Person* p = (Person*) _Person.new("Gabriel");
+  p->info(p);
 
   putchar('\n');
   p->eat(p, HoneyHam);
   putchar('\n');
 
-  _info(p);
+  p->info(p);
 
   putchar('\n');
   p->play(p, 10);
   putchar('\n');
 
-  _info(p);
+  p->info(p);
+
+  p = _Person.delete(p);
 
   return EXIT_SUCCESS;
 }
