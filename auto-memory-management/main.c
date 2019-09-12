2c7a34c9a59fec0187942a8f895d8d09ba714f25
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-11 23:10:38 -0300

    Imprimindo endereço de memoria apontado pelos ponteiros

--- auto-memory-management/main.c
+++ auto-memory-management/main.c
@@ -7,11 +7,11 @@ int main(int argc, char* argv[]) {
 
   int* my_intp = (int*) alloc(sizeof(int));
   *my_intp = 10;
-  printf("my_intp = %d\n", *my_intp);
+  printf("my_intp(%p) = %d\n", my_intp, *my_intp);
   
   char* my_strp = (char*) alloc(sizeof(char) * 100);
   strcpy(my_strp, "Ola, meu nome eh Gabriel!");
-  printf("my_strp = %s", my_strp);
+  printf("my_strp(%p) = %s", my_strp, my_strp);
 
   // Note que não estou chamando nenhum free para desalocar a memória alocada para os ponteiros
   return 0;
