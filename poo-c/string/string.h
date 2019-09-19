97cf7a5c33ccddccbeac23485eb91f59eeece429
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-18 22:00:41 -0300

    Adicionando novos ponteiros para funçao que representam metodos publicos da classe.

--- poo-c/string/string.h
+++ poo-c/string/string.h
@@ -1,6 +1,8 @@
 #ifndef _STRING_H_
 #define _STRING_H_
 
+#include <stdbool.h>
+
 // struct que contém os membro e funções-membro private do objeto 
 struct _string_private_members {
   size_t __size;
@@ -10,6 +12,9 @@ typedef struct _string_private_members Header;
 struct _string {
   char* ptr;
   size_t (*length)(void* object);
+  bool (*isEmpty)(void* object);
+  bool (*setString)(struct _string* object, char* new_str);
+  void (*info)(struct _string* object);
 };
 typedef struct _string String;
 
