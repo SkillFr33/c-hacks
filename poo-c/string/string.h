2f1729fa75adcaa7073247721a80bcb63ec88fd0
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-10-19 20:20:08 -0300

    Criando a interface da classe (membros privados, membros publicos, construtor e destrutor)

--- poo-c/string/string.h
+++ poo-c/string/string.h
@@ -3,8 +3,35 @@
 
 #include <stdbool.h>
 
+// syntax sugar para 'struct string_private_members*'
+typedef struct string_public_members*object; 
+
+// struct que contém os membros privados da classe string
+typedef struct string_private_members {
+  size_t size;
+  char*  buffer;
+} StringPrivateMembers;
 
 
+// struct que contém os memebros públicos da classe string
+typedef struct string_public_members {
+
+  // setters e getters
+  size_t (*get_size)(object this);
+  void (*set_string)(object this, const char* new_buffer);
+  const char* const (*get_string)(object this);
+
+  // métodos utilitários
+  char (*at)(object this, const int index);
+
+} StringPublicMembers;
+
+
+// struct que contém o contrutor e destrutor da classe string
+typedef struct class_string {
+  object (*new)(void);
+  void* (*delete)(object this);
+} class_string;
 
 
 #endif // _STRING_H_
