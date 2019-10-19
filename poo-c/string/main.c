86bdf379cf52ebba277fd24ebfc2adacfbf8b90d
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-10-19 19:30:33 -0300

    Refazendo tudo, antes estava meio confuso e nem eu sabia mais oq tava fazendo

--- poo-c/string/main.c
+++ poo-c/string/main.c
@@ -2,22 +2,9 @@
 #include <stdio.h>
 #include "string.h"
 
+
 int main() {
 
-  String* str = new_string("oi");
-  String* str2 = new_string("Ola");
 
-  str->info(str);
-  str->info(str2);
-
-  str->copy_string(str, str2);
-  str2->setString(str2, "eita, oi!?");
-
-  str->info(str);
-  str->info(str2);
-
-  str = delete_string(str);
-  str2 = delete_string(str2);
-
-  return 0;
+  return EXIT_SUCCESS;
 }
--- poo-c/string/string.h
+++ poo-c/string/string.h
@@ -3,23 +3,8 @@
 
 #include <stdbool.h>
 
-// struct que contém os membro e funções-membro private do objeto 
-struct _string_private_members {
-  size_t __size;
-};
-typedef struct _string_private_members Header;
 
-struct _string {
-  char* ptr;
-  size_t (*length)(void* this);
-  bool (*isEmpty)(void* this);
-  bool (*setString)(struct _string* this, char* new_str);
-  void (*info)(struct _string* this);
-  bool (*copy_string)(struct _string* this, struct _string* other);
-};
-typedef struct _string String;
 
-String* new_string(char* str);
-void* delete_string(struct _string* this);
+
 
 #endif // _STRING_H_
