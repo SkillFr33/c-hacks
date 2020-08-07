4727c05a7aca9f179d68ef4b59e57a0781a0dbe7
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-06 23:53:51 -0300

    Usando poll para monitorar socket de comunicação com o servidor e a entrada padrão. Fiz isso para validar mensagem de 'timeout' recebida do servidor

--- inet-socket/stream_socket/src/client/main.c
+++ inet-socket/stream_socket/src/client/main.c
@@ -2,6 +2,7 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
+#include <poll.h>
 #include "client.h"
 #include "../util/util.h"
 
@@ -18,33 +19,65 @@ int main(int argc, char* argv[]) {
   // conecta no host/servidor addr:port
   int sockfd = connect_tcp_server(addr, port);
 
+  // pollfd para monitorar evento de entrada
+  struct pollfd pfd[2];
+  pfd[0].fd = 0;          // entrada padrão
+  pfd[0].events = POLLIN; // evento de entrada
+
+  pfd[1].fd = sockfd; // socket p/ comunicar com servidor
+  pfd[1].events = POLLIN; // evento de entrada
+
   char buffer[1024]; // buffer para armazenar as mensagens enviadas/recebidas
   int bytes; // quantidade de bytes enviados/recebidos
 
   // lê uma mensagem da entrada padrão e envia para o servidor
-  while(printf("Mensagem( [CTRL+D] para sair): "), 
-        fgets(buffer, sizeof(buffer), stdin) != NULL) {
+  int poll_ret = 0;
+  while(fprintf(stderr, "Mensagem( [CTRL+D] para sair): "),
+        (poll_ret = poll(pfd, 2, -1)) != -1 ) {
     
-    // verifica se alguma coisa foi digitada
-    if(buffer[0] == '\n')
-      continue;
+    // adquire o fd que sofreu o evento
+    int fd_event = get_fd_by_event(pfd, 2, POLLIN);
+    if(fd_event == -1)
+      continue; // evento inesperado
 
-    replace(buffer, '\n', '\0');
+    // servidor me enviou alguma mensagem
+    if(fd_event == sockfd) {
+      bytes = recv(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
+      if(bytes == -1)
+        break;
+      
+      if(strncmp(buffer, "timeout", 7) == 0) {
+        puts("\nDesconectado por timeout!");
+        exit(0);
+      }
+    }
+    else { // evento de entrada na entrada padrão
 
-    // envia a mensagem para o servidor
-    bytes = send(sockfd, buffer, strlen(buffer) + 1, 0);
-    if(bytes == -1)
-      panic("send");
-    
-    // recebe uma mensagem do servidor
-    bytes = recv(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
-    if(bytes <= 0)
-      break;
-    
-    printf("Mensagem recebida: %s\n", buffer);
-  }
+      if(fgets(buffer, sizeof(buffer), stdin) == NULL)
+        break;
 
-  puts("\nDesconectado!");
+      // verifica se alguma coisa foi digitada
+      if(buffer[0] == '\n')
+        continue;
+
+      replace(buffer, '\n', '\0');
+
+      // envia a mensagem para o servidor
+      bytes = send(sockfd, buffer, strlen(buffer) + 1, 0);
+      if(bytes == -1)
+        panic("send");
+      
+      // recebe uma mensagem do servidor
+      bytes = recv(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
+      if(bytes <= 0)
+        break;
+      
+      printf("Mensagem recebida: %s\n", buffer);
+    }
+  } // end while
+
+  if(poll_ret == -1)
+    panic("poll");
 
   return EXIT_SUCCESS;
 }
