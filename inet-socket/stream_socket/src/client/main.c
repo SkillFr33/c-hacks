4a103865de6c73d704b20af57a71d09ea39991c1
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-10 20:39:17 -0300

    recv deixa de ocorrer logo após enviar uma mensagem ao servidor, essa responsabilidade passa a ser apenas do bloco if(fd_event == sockfd)

--- inet-socket/stream_socket/src/client/main.c
+++ inet-socket/stream_socket/src/client/main.c
@@ -41,8 +41,11 @@ int main(int argc, char* argv[]) {
   // lê uma mensagem da entrada padrão e envia para o servidor
   int epoll_ret = 0;
   struct epoll_event evs[2] = {0}; // STDIN_FILENO e sockfd
-  while(fprintf(stderr, "Mensagem( [CTRL+D] para sair): "),
-        (epoll_ret = epoll_wait(epoll_fd, evs, 2, -1)) != -1 ) {
+  puts("Digite suas mensagens( [CTRL+D] para sair)!\n");
+  while( (epoll_ret = epoll_wait(epoll_fd, evs, 2, -1)) != -1 ) {
+    
+    // zera o buffer pra evitar bugs
+    memset(buffer, 0, sizeof(buffer));
     
     // adquire o fd que sofreu o evento
     int fd_event = get_fd_by_event(evs, epoll_ret, EPOLLIN);
@@ -51,18 +54,28 @@ int main(int argc, char* argv[]) {
 
     // servidor me enviou alguma mensagem
     if(fd_event == sockfd) {
+      // recebe uma mensagem do servidor
       bytes = recv(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
-      if(bytes == 0) {
+
+      // bytes > 0, alguma mensagem foi recebida
+      if(bytes > 0) {
+
+        if(buffer[0] == '\0' || buffer[0] == '\n')
+          continue;
+        else if(strncmp(buffer, "timeout", 7) == 0) {
+          puts("\nDesconectado por timeout!");
+          exit(0);
+        }
+        else
+          printf("Mensagem recebida: %s\n", buffer);
+
+      }
+      else if(bytes == 0) { // nenhum byte recebido, servidor encerrou a conexão
         puts("\nServidor fechou!");
         exit(0);
       }
-      else if(bytes == -1)
-        break;
-      
-      if(strncmp(buffer, "timeout", 7) == 0) {
-        puts("\nDesconectado por timeout!");
-        exit(0);
-      }
+      else // bytes < 0, erro no recv
+        perror("recv");
     }
     else { // evento de entrada na entrada padrão
       if(fgets(buffer, sizeof(buffer), stdin) == NULL)
@@ -78,16 +91,6 @@ int main(int argc, char* argv[]) {
       bytes = send(sockfd, buffer, strlen(buffer) + 1, 0);
       if(bytes == -1)
         panic("send");
-      
-      // recebe uma mensagem do servidor
-      bytes = recv(sockfd, buffer, sizeof(buffer), MSG_NOSIGNAL);
-
-      if(bytes > 0)
-        printf("Mensagem recebida: %s\n", buffer);
-      else if(bytes == 0)
-        break; // servidor encerrou conexão
-      else
-        perror("recv"); // erro em recv
     }
   } // end while
 
